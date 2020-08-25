795790f669d56637d98c50266d5af9249e84084a
Swaroop Bajra <sarop_bajra@hotmail.com>
2020-08-26 08:11:40 +1000

    installed axios, Created components/BurningAirlines.js

---
+++ src/components/BurningAirlines.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class BurningAirlines extends React.Component {
+  render() {
+    return (
+      <div className="ba">
+        <h1>Burning Airlines</h1>
+      </div>
+    );
+  } // render
+} // class BurningAirlines
+export default BurningAirlines;
