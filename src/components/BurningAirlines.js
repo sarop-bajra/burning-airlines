9d646768c87d5c5f65c406142adb9e7b1773cb5e
Swaroop Bajra <sarop_bajra@hotmail.com>
2020-08-26 09:17:58 +1000

    fake api

--- src/components/BurningAirlines.js
+++ src/components/BurningAirlines.js
@@ -3,10 +3,36 @@ import axios from 'axios';
 
 
 class BurningAirlines extends React.Component {
+
+  state = {
+    flights: []
+  };
+
+  componentDidMount(){
+    console.log('Loaded');
+    axios.get('https://api.mocki.io/v1/b043df5a')
+    .then(res => {
+      console.log(res.data);
+      this.setState({flights: res.data});
+
+    })
+    .catch( err => console.warn(err));
+
+  }
+
   render() {
     return (
       <div className="ba">
         <h1>Burning Airlines</h1>
+          <div>
+          {
+            // this.state.flights.reverse().map( flight => {return <div className="flight" key={ flight.city }>{ flight.content }</div>})
+
+            this.state.flights.map( flight => {
+              return <li key={ flight.city }>{flight.name}</li>
+            }) // map
+          }
+          </div>
       </div>
     );
   } // render
