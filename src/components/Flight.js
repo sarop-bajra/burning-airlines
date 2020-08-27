f146d89ed0060877968f6564b92b32d8fa518fe6
Ryan Xin <xinpf0715@gmail.com>
2020-08-27 15:36:43 +1000

    show plane information on flight

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -8,17 +8,15 @@ const PLANES_URL = 'http://localhost:3000/planes';
 class Flight extends React.Component {
 
   state = {
-    origin:'',
-    destination:'',
+    origin: '',
+    destination: '',
     date: '',
     planeId: 0,
-    flights:[],
-    planes: [],
     name:'',
     columns: 0,
+    rows: 0,
     seatRow: 0,
-    seatColumn: 0,
-    rows: 0
+    seatColumn: 0
   };
 
 
@@ -31,7 +29,6 @@ class Flight extends React.Component {
       }
       table.push(<tr>{rows}</tr>)
     }
-
     return table
   } // create table
 
@@ -39,7 +36,6 @@ class Flight extends React.Component {
     console.log(ev.target.value);
     this.setState({seat:ev.target.value})
     console.log(this.state.seat);
-
   }
 
   componentDidMount(){
@@ -51,8 +47,18 @@ class Flight extends React.Component {
     })
     .then(res => {
       console.log(res.data);
+      this.setState({origin: res.data[0].origin});
+      this.setState({destination: res.data[0].destination});
+      this.setState({date: res.data[0].date});
+      this.setState({planeId: parseInt(res.data[0].plane_id)});
+      this.setState({name: res.data[0].plane.name});
+      this.setState({columns: res.data[0].plane.columns});
+      this.setState({rows: res.data[0].plane.rows});
+      console.log(res.data[0].plane.columns);
+      console.log(res.data[0].plane.rows);
     })
     .catch( err => console.warn(err));
+
 
   } // componentDidMount
 
@@ -61,19 +67,20 @@ class Flight extends React.Component {
       <div>
       <h2>Seat Map</h2>
         <p>Please choose an available seat</p>
+        <div>
+          <p>Date: {this.state.date}</p>
+          <p>From: {this.state.origin}</p>
+          <p>To: {this.state.destination}</p>
+        </div>
         <h3>{this.state.name}</h3>
         <div>
 
-
-        <table>
-          {this.createTable()}
-        </table>
-
-        <p>{this.state.seat}</p>
-
+          <table>
+            {this.createTable()}
+          </table>
+          <p>{this.state.seat}</p>
 
         </div>
-
 
       </div>
     ); // return
